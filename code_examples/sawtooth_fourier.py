41d32117d863718eec378d5e19183734505d3fe2
Juan I. Causse <jcausse@itba.edu.ar>
2026-07-02 00:16:21 -0300

    Updates

--- code_examples/sawtooth_fourier.py
+++ code_examples/sawtooth_fourier.py
@@ -3,9 +3,6 @@ import math
 from scipy import signal
 import numpy as np
 
-# x values
-x = np.linspace(-10, 10, 100000, endpoint=True)
-
 def generate(x, k):
     # y values initialization
     y = [0] * len(x)
@@ -19,29 +16,34 @@ def generate(x, k):
 
     return y, y_values
 
+def plot_common(title, filename, **kwargs):
+    plt.plot(x, np.pi * signal.sawtooth([i + np.pi for i in x]), color='black', linestyle='--')
+    plt.title(title)
+    plt.xlabel("t")
+    plt.ylabel("d(t)")
+    plt.grid()
+    plt.savefig(filename)
+    plt.close()
+
+# x values
+x = np.linspace(-10, 10, 100000, endpoint=True)
+
+###################################################################################
+##############################   PLOTS   ##########################################
+###################################################################################
+
 # Plot 1 (last value of K)
 K = 10
 y, _ = generate(x, K)
 
-plt.plot(x, np.pi * signal.sawtooth([i + np.pi for i in x]), color='black', linestyle='--')
+
 plt.plot(x, y)
-plt.title(f"Sawtooth Fourier Series (K={K})")
-plt.xlabel("x")
-plt.ylabel("d(t)")
-plt.grid()
-plt.savefig('sawtooth_fourier_last_k.png')
-plt.close()
+plot_common(f"Sawtooth Fourier Series (K={K})", 'img/sawtooth_fourier_last_k.png')
 
 # Plot 2 (all values of K)
 K = 4
 _, y_values = generate(x, K)
 
-plt.plot(x, np.pi * signal.sawtooth([i + np.pi for i in x]), color='black', linestyle='--')
 for k in y_values:
     plt.plot(x, y_values[k])
-plt.title(f"Sawtooth Fourier Series (K in {{{', '.join([str(k) for k in range(1, K + 1)])}}})")
-plt.xlabel("x")
-plt.ylabel("d(t)")
-plt.grid()
-plt.savefig('sawtooth_fourier_all_k.png')
-plt.close()
+plot_common(f"Sawtooth Fourier Series (K in {{{', '.join([str(k) for k in range(1, K + 1)])}}})", 'img/sawtooth_fourier_all_k.png')
